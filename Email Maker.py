6ea07a1acd39dc2411c4735ec454a97aa1164173
yns aml <111573387+avizero777@users.noreply.github.com>
2022-08-29 12:17:02 -0700

    Update Email Maker.py

--- Email Maker.py
+++ Email Maker.py
@@ -1,32 +1,33 @@
-def email(excel_file):
+def email(exl_file):
     from openpyxl import load_workbook
-    book = load_workbook(excel_file)
+    book = load_workbook(exl_file)
     sh = book.active
+    
     class person:
         def __init__(self, name, surname, gender):
             self.name = name
             self.surname = surname
             self.gender = gender
+    
     for i in range(2, sh.max_row+1):
-        print("\n")
-        for j in range(1, sh.max_column):
+        for j in range(1, sh.max_column+1):
             cell_obj = sh.cell(row=i, column=j)
             if j == 1 :
                 first_name = cell_obj.value
             elif j == 2 :
                 nick_name = cell_obj.value
-            else:
+            elif j == 3 :
                 gnd = cell_obj.value
-        data = person(first_name, nick_name, gnd)
-        emails_file = open('emails', 'a')
-        if gnd == 'female':
-            emails_file.write('\n' + 'Miss.' + data.name +'.'+ data.surname + '@email.com')
-        elif gnd == 'male':
-            emails_file.write('\n' + 'Mr.' + data.name +'.'+ data.surname + '@email.com')
-        emails_file.close
+            elif j == 4 :
+                data = person(first_name, nick_name, gnd)      
+                if gnd == 'female':
+                   cell_obj.value = ('\n' + 'Miss.' + data.name +'.'+ data.surname + '@email.com')
+                elif gnd == 'male':
+                    cell_obj.value = ('\n' + 'Mr.' + data.name +'.'+ data.surname + '@email.com')
     
+        book.save(filename=exl_file)
 
 try:
-    email(r'   .xlsx') # <===  """"enter your excel file with the absolute path """"
+    email(r'.xlsx')  # <===  """"enter your excel file with the absolute path """"
 except:
     print('FileNotFoundError')
